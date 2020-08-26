32364ab8bc326d930df1156b8c5a12c7d963a51b
Timi20 <timileyin.ogunsola@teamapt.com>
2020-08-26 06:20:47 +0100

    fix: premMissingElement now passes all constraint

--- src/codility/PermMissingElem.kt
+++ src/codility/PermMissingElem.kt
@@ -5,25 +5,43 @@ fun main() {
         // write your code in Kotlin
         A.sort()
         var j = 1
-        if (A.size == 1 ){
-            return A[0]
-        }
-        if (A.isEmpty()){
-            return 0
+        if (A.isEmpty()) {
+            return j
         }
 
-        if (A.size == 2){
-            if (A[0] < A[1]){
-                return A[0] + 1
+        if (A.size == 1) {
+            if(A[0] == 1){
+                return A[0]+1
             } else {
-                return A[1] + 1
+                return A[0] - 1
             }
         }
 
-        for (i in 0..A.lastIndex - 1) {
+        if (A.size == 2) {
+            return if (A[1] - A[0] == 1 && A[0] != 1) {
+                A[0] - 1
+            } else if (A[1] - A[0] != 1){
+                A[0]+1
+            } else {
+                A[1] + 1
+            }
+        }
+
+        for (i in 0 until A.lastIndex) {
             if ((A[i + 1] - A[i]) > 1) {
-                j = A[i] + 1
+                j = if (((i + 2) <= A.lastIndex) && (A[i + 2] - A[i + 1]) == 1) {
+                    A[i + 1] - 1
+                } else {
+                    A[i] + 1
+                }
                 break
+            } else {
+                j = if (A[0] == 1){
+                    A[A.lastIndex] + 1
+                } else {
+                    A[0] - 1
+                }
+
             }
         }
         return j
